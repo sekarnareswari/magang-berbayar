f2de9aba7d5e85ed9e46084a0fae3d13986ef942
sekar <sekarnareswari@berkeley.edu>
2024-06-26 12:27:02 -0700

    removed files to make website static

--- pages/api/jobs.js
+++
@@ -1,8 +0,0 @@
-// pages/api/jobs.js
-import { connectToDatabase } from "../../lib/mongodb";
-
-export default async (req, res) => {
-    const { db } = await connectToDatabase();
-    const jobs = await db.collection("jobs").find({}).toArray();
-    res.status(200).json(jobs);
-};
